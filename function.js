5b0084f4b06c12a8d1a7b7f7a73663424df89a80
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-15 14:09:42 +0900

    Create function.js

---
+++ function.js
@@ -0,0 +1,7 @@
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson +  " and I'm " + age);
+}
+
+sayHello("minsu", 10);
+sayHello("chulsu", 23);
+sayHello("hansu", 21);
