f440990a736b9c8a1132268cc813fbb62de6b9d1
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-15 14:14:52 +0900

    Create function2.js

---
+++ function2.js
@@ -0,0 +1,11 @@
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+plus(60, 8);
+divide(98, 20);
