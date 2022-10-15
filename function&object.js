b1ff65c5fbbdce6809e12bfd277cf50cf06158f7
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-15 14:19:16 +0900

    Create function&object.js

---
+++ function&object.js
@@ -0,0 +1,10 @@
+// function + Object
+const player = {
+    name : "minsung",
+    sayHello: function (otherPersonName) {
+        console.log("hello " + otherPersonName + " nice to meet you!")       
+    }
+};
+
+player.sayHello("minsung");
+player.sayHello("untitled");
