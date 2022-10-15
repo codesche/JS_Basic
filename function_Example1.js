25c13ddd5321865e6a4f358a1d82a1f0f0ec7ab3
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-15 14:50:25 +0900

    Create function_Example1.js

---
+++ function_Example1.js
@@ -0,0 +1,29 @@
+const calculator = {
+    add: function(a, b) {
+        console.log(a + b);
+    },
+
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+
+    multiple: function(a, b) {
+        console.log(a * b);
+    },
+
+    division: function(a, b) {
+        console.log(a / b);
+    },
+
+    pow: function(a, b) {
+        console.log(a ** b);
+    },
+};
+
+calculator.add(7, 2);
+calculator.minus(7, 2);
+calculator.multiple(7, 2);
+calculator.division(7, 2);
+calculator.pow(7, 2);
+
+
