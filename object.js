f00efc097fd8c34e00a73ea8a46de25f88d183af
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-15 12:51:56 +0900

    Create object.js

---
+++ object.js
@@ -0,0 +1,10 @@
+const player = {
+    name : "minsung",
+    points: 10,
+    fat: true,
+};
+
+console.log(player.fat);
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player.points);
